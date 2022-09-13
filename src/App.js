4a0b4e3a9dcf0dec22e79391ce880469ba87a07d
Willy <leaguematter@likelion.org>
2022-09-13 17:59:06 +0900

    js first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import requests from './request';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [buckets, setBuckets] = useState(new Set());
+
+  const url = 'https://api.themoviedb.org/3';
+  
+  useEffect(() => {
+    async function getMoives() {
+      const response = await fetch(`${url}${requests.fetchTrending}`);  
+      const movieList = await response.json();
+      setMovies(movieList.results);
+    }
+    getMoives();
+  }, []);
+
+  const onClickHandler = (event) => {
+    const movie = event.target;
+    console.dir(movie);
+  }
+
+  // console.log(movieList[2]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* MovieList */}
+        {movies.map(movie => {
+          return (
+            <div key={movie.id} onClick={onClickHandler}>
+              <div>{movie.title}</div>
+              <div>{movie.original_title}</div>
+            </div>
+          );
+        })}
+      {/* BucketList */}
+
     </div>
   );
 }
