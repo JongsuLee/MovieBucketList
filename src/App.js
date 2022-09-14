26d6d049503007ec82962e002817f172de49c4f0
Willy <leaguematter@likelion.org>
2022-09-14 11:05:34 +0900

    struggling with bucketState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {useEffect, useState} from 'react';
 import './App.css';
 import requests from './request';
-import MovieList from './components/MovieList';
+import MovieList from './components/MovieList/MovieList';
+import BucketList from './components/BucketList/BucketList';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [buckets, setBuckets] = useState(new Set());
+  const [bucketSet, setBucketSet] = useState(new Set());
+  const [buckets, setBuckets] = useState([]);
 
   const url = 'https://api.themoviedb.org/3';
   
@@ -21,9 +23,11 @@ function App() {
   const onClickHandler = (id) => {
     const movieId = Number(id);
     for (let movie of movies) {
-      if (movie.id === movieId) setBuckets(buckets.add(movie));  
+      if (movie.id === movieId) {
+        setBucketSet(new Set([...buckets, movie]));
+        setBuckets([...bucketSet]);
+      }  
     }
-    console.log(buckets);
   }
 
 
@@ -32,6 +36,7 @@ function App() {
       {/* MovieList */}
       <MovieList movieList={movies} onClickHandler={onClickHandler}/>
       {/* BucketList */}
+      <BucketList bucketList={buckets}/>
     </div>
   );
 }
--- src/components/BucketList/BucketList.js
+++ src/components/BucketList/BucketList.js
@@ -1,18 +1,20 @@
 import Bucket from "./Bucket";
 
-/**
- * BucketList
- * 
- * Bucket1
- * Bucket2
- * 
- */
 
 const BucketList = (props) => {
-
     const bucketList = props.bucketList;
-    bucketList.map(bucket => {
-        return <Bucket bucket={bucket}/>
-    })
-    console.log();
+    console.log(bucketList);
+    return (
+        <div className="BucketList">
+            <div>1</div>
+            {/* {bucketList.map(bucket => {
+                return <Bucket object={bucket}/>
+            })} */}
+            {bucketList.map(bucket => {
+                return <Bucket test={bucket}/>
+            })}
+        </div>
+    );
 }
+
+export default BucketList;
